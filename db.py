ae3f2764b56cc57f085dca587f7bfce39f11bf7d
Ram Basnet <rambasnet@users.noreply.github.com>
2022-11-08 10:22:23 -0700

    update db.py

--- db.py
+++ db.py
@@ -1,17 +1,60 @@
+__author__ = "Ram Basnet"
+__date__ = "Nov 8, 2022"
+
+"""
+DB module 
+- contains utility functions to work with sqlite db
+"""
 import sqlite3
+import settings
 
 # create_connection function
-def create_connection(db_file):
+def create_connection(db_file: str):
   """
   function to connect to a sqlite db with given file db_file
   db_file: database file name
-  return: None
+  return: None or conn
   """
   conn = None
-  conn = sqlite3.connect(db_file)
-  print('connection successful...')
-  print(sqlite3.version)
-  conn.close()
+  try:
+    conn = sqlite3.connect(db_file)
+    print('Connection successful...')
+    return conn
+  except Exception as ex:
+    print('Error: ', ex, db_file)
+  return None
+
+def create_table(conn, sql:str) -> bool:
+  """
+  conn: sqlite connnection object
+  sql: create table sql string
+  return: True if table created successfully, False otherwise
+  """
+  try:
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    return True
+  except Exception as ex:
+    print('Error:', ex, sql)
+    return False
+  
+def test():
+  conn = create_connection(settings.DB_NAME)
+  with conn:
+    sql = """
+          CREATE TABLE IF NOT EXISTS projects (
+            id integer primary key,
+            name text NOT NULL,
+            begin_date text,
+            end_date text
+          );
+          """
+    success = create_table(conn, sql)
+    if success:
+      print('Project table created successfully!')
+    else:
+      print('Project table could not be created!')
+  
 
 if __name__ == "__main__":
-  create_connection('gradebook.db')
+  test()

92fc1f354a3b5696f4095c571558f368ba95f65c
Ram Basnet <rambasnet@users.noreply.github.com>
2022-11-08 10:46:00 -0700

    update db.py

--- db.py
+++ db.py
@@ -37,8 +37,8 @@ def create_table(conn, sql:str) -> bool:
   except Exception as ex:
     print('Error:', ex, sql)
     return False
-  
-def test():
+
+def create_table_projects():
   conn = create_connection(settings.DB_NAME)
   with conn:
     sql = """
@@ -55,6 +55,66 @@ def test():
     else:
       print('Project table could not be created!')
   
+def create_table_tasks():
+  conn = create_connection(settings.DB_NAME)
+  sql = """
+          -- tasks table
+          CREATE TABLE IF NOT EXISTS tasks (
+            id integer PRIMARY KEY,
+            name text NOT NULL,
+            priority integer,
+            project_id integer NOT NULL,
+            status_id integer NOT NULL,
+            begin_date text NOT NULL,
+            end_date text NOT NULL,
+            FOREIGN KEY (project_id) REFERENCES projects (id)
+          );
+          """
+    
+  with conn:
+    success = create_table(conn, sql)
+    if success:
+      print('Tasks table created successfully!')
+    else:
+      print("Tasks table couldn't be created!")
+    
+def test():
+  conn = create_connection(settings.DB_NAME)
+  with conn:
+    sql = """
+          CREATE TABLE IF NOT EXISTS projects (
+            id integer primary key,
+            name text NOT NULL,
+            begin_date text,
+            end_date text
+          );
+          """
+    success = create_table(conn, sql)
+    if success:
+      print('Project table created successfully!')
+    else:
+      print('Project table could not be created!')
+
+    sql = """
+          -- tasks table
+          CREATE TABLE IF NOT EXISTS tasks (
+            id integer PRIMARY KEY,
+            name text NOT NULL,
+            priority integer,
+            project_id integer NOT NULL,
+            status_id integer NOT NULL,
+            begin_date text NOT NULL,
+            end_date text NOT NULL,
+            FOREIGN KEY (project_id) REFERENCES projects (id)
+          );
+          """
+    success = create_table(conn, sql)
+    if success:
+      print('Tasks table created successfully!')
+    else:
+      print("Tasks table couldn't be created!")
 
 if __name__ == "__main__":
   test()
+  create_table_projects()
+  create_table_tasks()
